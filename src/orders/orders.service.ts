d26fa7f0708f0cd7f3804b9160e2423f390e33ae
Weber Dantas <wdantas89@gmail.com>
2022-08-18 22:44:20 -0300

    Criação das apis de produtos e pedidos

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { In, Repository } from 'typeorm';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
+
+@Injectable()
+export class OrdersService {
+  constructor(
+    @InjectRepository(Order) private orderRepo: Repository<Order>,
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto) {
+    const order = this.orderRepo.create(createOrderDto);
+    const products = await this.productRepo.find({
+      where: { id: In(order.items.map((item) => item.product_id)) },
+    });
+
+    order.items.forEach((item) => {
+      const product = products.find(
+        (product) => product.id === item.product_id,
+      );
+      item.price = product.price;
+    });
+
+    return this.orderRepo.save(order);
+  }
+
+  findAll() {
+    return `This action returns all orders`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} order`;
+  }
+
+  update(id: number, updateOrderDto: UpdateOrderDto) {
+    return `This action updates a #${id} order`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} order`;
+  }
+}
